06ad923bb9b2af5eed65c3aa9b9b0dc118708312
tranhuuloc2010 <tranhuuloc2010@gmail.com>
2023-03-15 20:21:47 +0700

    update hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from "@nestjs/common";
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3434);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
